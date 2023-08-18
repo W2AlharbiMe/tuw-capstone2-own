0b9cf9073c374cb321551e1f6b57307cfcae4572
W2AlharbiMe <w2.alharbi.me@gmail.com>
2023-08-18 09:36:56 +0300

    init commit

---
+++ src/test/java/com/example/capstone2/Capstone2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capstone2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Capstone2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
